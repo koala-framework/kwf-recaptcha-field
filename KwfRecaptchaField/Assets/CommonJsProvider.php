053ffcba560fb7061c68b9f942dd128641aaae06
Alexander Kaufmann <ak@vivid-planet.com>
2018-05-22 10:03:55 +0200

    use correct length for provider check

--- KwfRecaptchaField/Assets/CommonJsProvider.php
+++ KwfRecaptchaField/Assets/CommonJsProvider.php
@@ -3,7 +3,7 @@ class KwfRecaptchaField_Assets_CommonJsProvider extends Kwf_Assets_Provider_Abst
 {
     public function getDependency($dependencyName)
     {
-        if (substr($dependencyName, 0, 11) == 'kwfRecaptchaField') {
+        if (substr($dependencyName, 0, 17) == 'kwfRecaptchaField') {
             if (substr($dependencyName, -3) != '.js') {
                 return new Kwf_Assets_Dependency_File_Js($this->_providerList, $dependencyName.'.js');
             }
